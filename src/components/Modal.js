fa2e36d5803be3e8f4d363f2edb0915dea4b75b1
lilyhoratio <lilyhzhou@gmail.com>
2020-02-09 18:51:13 -0800

    refresh page for new random integer on modal button click working

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import * as api from "../services/api";
 
 function Modal({ isGameOver, isGameWon, integerCombo }) {
   if (!isGameOver) return null;
@@ -14,10 +15,15 @@ function Modal({ isGameOver, isGameWon, integerCombo }) {
       ) : (
         <div className="modal-content">
           <p>You lose! The correct number combination was {integerCombo}.</p>
-          <div>( ͡° ʖ̯ ͡°) ┏༼ ◉╭╮◉༽┓ </div>
+          <div>( ͡° ʖ̯ ͡°) ┏༼ ◉╭╮◉༽┓ ( ⚈̥̥̥̥̥́⌢⚈̥̥̥̥̥̀) </div>
         </div>
       )}
-      <div className="generate-number-button">Generate new random number</div>
+      <div
+        className="generate-number-button"
+        onClick={() => window.location.reload(false)}
+      >
+        Generate new random number
+      </div>
     </div>
   );
 }
